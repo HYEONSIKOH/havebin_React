9461e7b303a8c4561f1ea090fbe38b2d54bf3e17
HYEONSIK <ks06891@naver.com>
2024-05-14 03:12:38 +0900

    package.json 수정

--- src/JS/main.js
+++ src/JS/main.js
@@ -4,7 +4,7 @@ import { MOBILE } from './mobile.js';
 
 
 function MAIN() {
-    const URL = 'http://192.168.11.150:8080/';
+    const URL = 'http://ec2-43-201-28-219.ap-northeast-2.compute.amazonaws.com:8080/';
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
     const isAndroid = /android/i.test(userAgent);
     const isiOS = /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream;
